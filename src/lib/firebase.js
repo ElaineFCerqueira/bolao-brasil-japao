10384c6e070fbb78fcfafac38a8e6ab7dd37ebab
Nane Cerqueira <108476624+ElaineFCerqueira@users.noreply.github.com>
2026-06-26 22:11:57 -0300

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,2 +1,14 @@
-// arquivo mantido para compatibilidade - configuração movida para firestore.js
-export default {};
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCP0SERdeuTpc08GBP0MuujFkgQqGG36ts",
+  authDomain: "bolao-brasil-japao.firebaseapp.com",
+  projectId: "bolao-brasil-japao",
+  storageBucket: "bolao-brasil-japao.firebasestorage.app",
+  messagingSenderId: "649514126116",
+  appId: "1:649514126116:web:b7eee7e701f585646e01b1"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
